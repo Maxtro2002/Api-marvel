11892aea613db04d5b951e2329f3389f3f4671c3
Maxtro2002 <jspr200231@gmail.com>
2023-03-26 15:06:49 -0500

    Paginas creadas

---
+++ lib/pages/characterList.dart
@@ -0,0 +1,135 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:api_marvel/pages/characterDetail.dart';
+import 'package:api_marvel/widgets/characterTitle.dart';
+
+import '../controller/character.dart';
+
+class ListCharacters extends StatefulWidget {
+  @override
+  _ListCharactersState createState() => _ListCharactersState();
+}
+
+class _ListCharactersState extends State<ListCharacters> {
+  List<dynamic> characters = [];
+
+  Future<void> getCharacters() async {
+    const ts = '1036022400';
+    const apiKey = '3120a5b21313225d07f1e4964712e341';
+    const hash = '70370070e0609a0ee48e5e9058a0059c';
+
+    const url =
+        'https://gateway.marvel.com:443/v1/public/characters?ts=$ts&apikey=$apiKey&hash=$hash&limit=50&offset=500';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      setState(() {
+        characters = jsonData['data']['results'];
+      });
+    } else {
+      throw Exception('Failed to load characters');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCharacters();
+  }
+
+  void _showCharacterDetail(int index) {
+    final character = characters[index];
+    final name = character['name'];
+    final thumbnail = character['thumbnail']['path'] +
+        '.' +
+        character['thumbnail']['extension'];
+    final description = character['description'];
+    // Validación para cuando description sea nulo o esté vacío
+    const defaultDescription = 'Este personaje no tiene descripción.';
+    final validDescription = (description == null || description.isEmpty)
+        ? defaultDescription
+        : description;
+    final comics = character['comics']['available'];
+    final series = character['series']['available'];
+    final stories = character['stories']['available'];
+    final events = character['events']['available'];
+    final List<dynamic> seriesList = character['series']['items'];
+    final seriesCount = min(seriesList.length, 3); // Validación para seriesList
+    final List<String> seriesNames = seriesList
+        .sublist(0, seriesCount)
+        .map((item) => item['name'].toString())
+        .toList();
+    final Character selectedCharacter = Character(
+        name: name,
+        image: thumbnail,
+        thumbnail: thumbnail,
+        description: validDescription,
+        comicsCount: comics,
+        seriesCount: series,
+        storiesCount: stories,
+        eventsCount: events,
+        firstThreeSeriesNames: seriesNames);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => CharacterDetails(
+                character: selectedCharacter,
+              )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 150),
+          child: SizedBox(
+            height: 50,
+            child: Image.asset(
+              'assets/images/marvel_logo.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: characters.isEmpty
+            ? const CircularProgressIndicator()
+            : GridView.builder(
+                padding: EdgeInsets.all(10),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemCount: characters.length,
+                itemBuilder: (context, index) {
+                  final character = characters[index];
+                  final thumbnail = character['thumbnail']['path'] +
+                      '.' +
+                      character['thumbnail']['extension'];
+                  final name = character['name'];
+                  return GestureDetector(
+                      onTap: () {
+                        _showCharacterDetail(index);
+                      },
+                      child: CharacterTile(
+                        character: characters[index],
+                        onTap: () {
+                          _showCharacterDetail(index);
+                        },
+                      ));
+                },
+              ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/characterTitle.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CharacterTile extends StatelessWidget {
+  final Map<String, dynamic> character;
+  final VoidCallback onTap;
+
+  const CharacterTile({Key? key, required this.character, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final name = character['name'];
+    final thumbnail = character['thumbnail']['path'] +
+        '.' +
+        character['thumbnail']['extension'];
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: DecorationImage(
+                    image: NetworkImage(thumbnail),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
